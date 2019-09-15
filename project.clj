074a5d9d69c1ed953fa7c8a12b5e08f76c6d9ef8
Sean Dawson <spdawson@agiledigital.com.au>
2019-09-15 21:00:13 +1000

    Release v1.0.13

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mule-metadata-extractor "1.0.12"
+(defproject mule-metadata-extractor "1.0.13"
   :description "Command line tools for processing Mule XML files"
   :url "https://github.com/NoxHarmonium/mule-preview"
   :license {:name "Apache License"
